684bc83486a5796b8e37ae9c6035a06c83e68e12
omid <Omidfoladvand0919@gmail.com>
2026-08-08 16:18:41 +0330

    Updates

--- assets/NotFound-DZfgHaC4.js
+++ assets/NotFound-DZfgHaC4.js
@@ -1,3 +1,3 @@
-import{n as e,t}from"./index-B3PgUS6K.js";var n=e(),r=t.div`
+import{i as e,t}from"./index-Dp97K4UI.js";var n=e(),r=t.div`
   background-color: red;
 `;function i(){return(0,n.jsx)(r,{children:`NotFound`})}export{i as default};
