710c91bcc4b2557a6b17518b50f63d716c9aa5c6
omid <Omidfoladvand0919@gmail.com>
2026-08-09 14:46:40 +0330

    Updates

--- assets/NotFound-CA46XeTY.js
+++ assets/NotFound-CA46XeTY.js
@@ -1,3 +1,3 @@
-import{i as e,t}from"./index-B2aJ6qWM.js";var n=e(),r=t.div`
+import{a as e,n as t}from"./index-6AGRPxH8.js";var n=e(),r=t.div`
   background-color: red;
 `;function i(){return(0,n.jsx)(r,{children:`NotFound`})}export{i as default};
--- assets/index-6AGRPxH8.js
+++ assets/index-6AGRPxH8.js
@@ -25,12 +25,15 @@ Please change the parent <Route path="${e}"> to <Route path="${e===`/`?`*`:`${e}
     background: var(--color-neutral-800);
     justify-content: space-evenly;
     border-radius: 12px;
+    color: var(--color-base-background);
   }
-`,Ya=L.div`
+`,Ya=L(Nn)`
   display: flex;
   gap: 8px;
   cursor: pointer;
   font-weight: var(--font-weight-bold);
+  color: var(--color-base-secondary);
+
   transition: all 0.3s ease;
   &:hover {
     color: var(--color-base-primary);
@@ -40,9 +43,10 @@ Please change the parent <Route path="${e}"> to <Route path="${e===`/`?`*`:`${e}
     padding:  0 12px;
      
   }
-`,Xa=L.div`
+`,Xa=L(Nn)`
   padding-left: 6px;
   border-left: 2px solid var(--color-base-secondary);
+  color: var(--color-base-secondary);
   cursor: pointer;
   font-weight: var(--font-weight-bold);
   transition: all 0.3s ease;
@@ -81,7 +85,7 @@ Please change the parent <Route path="${e}"> to <Route path="${e===`/`?`*`:`${e}
   align-items: center;
   justify-content: center;
   font-size: var(--font-body-sm);
-`;function $a({isMobile:e=!1}){return(0,d.jsxs)(Ja,{children:[(0,d.jsx)(Xa,{children:`ثبت نام`}),(0,d.jsxs)(Ya,{children:[`ورود`,(0,d.jsx)(Ka,{})]}),(0,d.jsxs)(Za,{children:[(0,d.jsx)(qa,{}),(0,d.jsx)(Qa,{children:`0`})]})]})}var eo=L.header`
+`;function $a({isMobile:e=!1}){return(0,d.jsxs)(Ja,{children:[(0,d.jsx)(Xa,{children:`ثبت نام`}),(0,d.jsxs)(Ya,{to:`Sign-in`,children:[`ورود`,(0,d.jsx)(Ka,{color:`var(--color-base-secondary)`})]}),(0,d.jsxs)(Za,{children:[(0,d.jsx)(qa,{}),(0,d.jsx)(Qa,{children:`0`})]})]})}var eo=L.header`
   width: 100%;
 `,to=L.div`
   width: 30px;
@@ -111,7 +115,7 @@ Please change the parent <Route path="${e}"> to <Route path="${e===`/`?`*`:`${e}
   flex-direction: column;
   margin: var(--header-margin-medium);
 `,io=L.div`
-  width: 80%;
+  width: 90%;
   display: flex;
   align-items: flex-start;
   justify-content: space-between;
@@ -166,7 +170,7 @@ Please change the parent <Route path="${e}"> to <Route path="${e===`/`?`*`:`${e}
     display: none;
   }
 `,fo=L.div`
-  width: 80%;
+  width: 90%;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -259,7 +263,7 @@ Please change the parent <Route path="${e}"> to <Route path="${e===`/`?`*`:`${e}
     display: ${({$open:e})=>e?`block`:`none`};
     width: 100%;
   }
-`,ho=[`مردانه`,`زنانه`,`بچگانه`,`لوازم ورزشی`,`شیکر و جاگ`];function go(){let[e,t]=(0,x.useState)(!1);return(0,d.jsxs)(eo,{children:[(0,d.jsx)(to,{onClick:()=>t(e=>!e),children:(0,d.jsx)(Wa,{})}),(0,d.jsx)(no,{children:`با عضویت در فیت لند اولین ارسال را مهمان ما باشید :)`}),(0,d.jsxs)(ro,{children:[(0,d.jsxs)(io,{children:[(0,d.jsxs)(ao,{children:[(0,d.jsx)(oo,{src:za,alt:`Fit Land Logo`}),(0,d.jsx)(so,{children:`فروشگاه لوازم ورزشی فیت لند`})]}),(0,d.jsxs)(co,{children:[(0,d.jsx)(Ba,{color:`var(--color-base-primary)`}),(0,d.jsx)(lo,{placeholder:`جستجو`})]}),(0,d.jsx)(uo,{children:(0,d.jsx)($a,{})})]}),(0,d.jsxs)(fo,{$open:e,children:[e&&(0,d.jsx)(Ga,{setIsOpen:t}),(0,d.jsx)(R,{children:ho.map((e,t)=>(0,d.jsx)(po,{children:e},t))}),(0,d.jsxs)(mo,{children:[(0,d.jsxs)(z,{children:[(0,d.jsx)(Va,{}),`جدیدترین محصولات`]}),(0,d.jsxs)(z,{children:[(0,d.jsx)(Ua,{}),`تخفیفات ویژه`]}),(0,d.jsxs)(z,{children:[(0,d.jsx)(Ha,{}),`پرفروش ترین ها`]})]}),(0,d.jsx)(B,{$open:e,children:(0,d.jsx)($a,{isMobile:!0})})]})]})]})}var _o=L.div`
+`,ho=[`مردانه`,`زنانه`,`بچگانه`,`لوازم ورزشی`,`شیکر و جاگ`];function go(){let[e,t]=(0,x.useState)(!1),n=yt();return n.pathname===`/Sign-in`||n.pathname===`/Sign-up`?null:(0,d.jsxs)(eo,{children:[(0,d.jsx)(to,{onClick:()=>t(e=>!e),children:(0,d.jsx)(Wa,{})}),(0,d.jsx)(no,{children:`با عضویت در فیت لند اولین ارسال را مهمان ما باشید :)`}),(0,d.jsxs)(ro,{children:[(0,d.jsxs)(io,{children:[(0,d.jsxs)(ao,{children:[(0,d.jsx)(oo,{src:za,alt:`Fit Land Logo`}),(0,d.jsx)(so,{children:`فروشگاه لوازم ورزشی فیت لند`})]}),(0,d.jsxs)(co,{children:[(0,d.jsx)(Ba,{color:`var(--color-base-primary)`}),(0,d.jsx)(lo,{placeholder:`جستجو`})]}),(0,d.jsx)(uo,{children:(0,d.jsx)($a,{})})]}),(0,d.jsxs)(fo,{$open:e,children:[e&&(0,d.jsx)(Ga,{setIsOpen:t}),(0,d.jsx)(R,{children:ho.map((e,t)=>(0,d.jsx)(po,{children:e},t))}),(0,d.jsxs)(mo,{children:[(0,d.jsxs)(z,{children:[(0,d.jsx)(Va,{}),`جدیدترین محصولات`]}),(0,d.jsxs)(z,{children:[(0,d.jsx)(Ua,{}),`تخفیفات ویژه`]}),(0,d.jsxs)(z,{children:[(0,d.jsx)(Ha,{}),`پرفروش ترین ها`]})]}),(0,d.jsx)(B,{$open:e,children:(0,d.jsx)($a,{isMobile:!0})})]})]})]})}var _o=L.div`
   display: flex;
   flex-direction: column;
   gap: 12px;
@@ -458,4 +462,4 @@ Please change the parent <Route path="${e}"> to <Route path="${e===`/`?`*`:`${e}
     gap: 24px;
     width: 90%;
   }
-`;function as(){return(0,d.jsx)(rs,{children:(0,d.jsxs)(is,{children:[(0,d.jsx)(Eo,{}),(0,d.jsx)(So,{}),(0,d.jsx)(Bo,{}),(0,d.jsxs)(`div`,{children:[(0,d.jsx)(Mo,{}),(0,d.jsx)(Xo,{}),(0,d.jsx)(ns,{})]})]})})}function os(){return(0,d.jsx)(`div`,{children:`Loading`})}function ss(){let{pathname:e}=yt();return(0,x.useEffect)(()=>{window.scrollTo({top:0,behavior:`smooth`})},[e]),null}var cs=(0,x.lazy)(()=>te(()=>import(`./Home-BQ8HBrUw.js`),[])),ls=(0,x.lazy)(()=>te(()=>import(`./NotFound-DCviGyks.js`),[])),us={HOME:`/`,NOTFOUND:`*`},ds=[{path:us.HOME,element:(0,d.jsx)(cs,{})},{path:us.NOTFOUND,element:(0,d.jsx)(ls,{})}],fs=(0,x.memo)(()=>(0,d.jsxs)(x.Suspense,{fallback:(0,d.jsx)(os,{}),children:[(0,d.jsx)(ss,{}),(0,d.jsxs)(qt,{children:[ds.map(({path:e,element:t})=>(0,d.jsx)(Gt,{path:e,element:t},e)),(0,d.jsx)(Gt,{path:`*`,element:(0,d.jsx)(ls,{})})]})]}));fs.displayName=`AppRoutes`,(0,S.createRoot)(document.getElementById(`root`)).render((0,d.jsx)(x.StrictMode,{children:(0,d.jsxs)(Mn,{children:[(0,d.jsx)(go,{}),(0,d.jsx)(f,{}),(0,d.jsx)(fs,{}),(0,d.jsx)(as,{})]})}));export{o as a,u as i,Nn as n,c as o,m as r,L as t};
+`;function as(){let e=yt();return e.pathname===`/Sign-in`||e.pathname===`/Sign-up`?null:(0,d.jsx)(rs,{children:(0,d.jsxs)(is,{children:[(0,d.jsx)(Eo,{}),(0,d.jsx)(So,{}),(0,d.jsx)(Bo,{}),(0,d.jsxs)(`div`,{children:[(0,d.jsx)(Mo,{}),(0,d.jsx)(Xo,{}),(0,d.jsx)(ns,{})]})]})})}function os(){return(0,d.jsx)(`div`,{children:`Loading`})}function ss(){let{pathname:e}=yt();return(0,x.useEffect)(()=>{window.scrollTo({top:0,behavior:`smooth`})},[e]),null}var cs=(0,x.lazy)(()=>te(()=>import(`./Home-3Z1fy0an.js`),[])),ls=(0,x.lazy)(()=>te(()=>import(`./SignIn-_tlDkF7g.js`),[])),us=(0,x.lazy)(()=>te(()=>import(`./NotFound-CA46XeTY.js`),[])),ds={HOME:`/`,SIGNIN:`/Sign-in`,NOTFOUND:`*`},fs=[{path:ds.HOME,element:(0,d.jsx)(cs,{})},{path:ds.NOTFOUND,element:(0,d.jsx)(us,{})},{path:ds.SIGNIN,element:(0,d.jsx)(ls,{}),layout:null}],ps=(0,x.memo)(()=>(0,d.jsxs)(x.Suspense,{fallback:(0,d.jsx)(os,{}),children:[(0,d.jsx)(ss,{}),(0,d.jsxs)(qt,{children:[fs.map(({path:e,element:t})=>(0,d.jsx)(Gt,{path:e,element:t},e)),(0,d.jsx)(Gt,{path:`*`,element:(0,d.jsx)(us,{})})]})]}));ps.displayName=`AppRoutes`,(0,S.createRoot)(document.getElementById(`root`)).render((0,d.jsx)(x.StrictMode,{children:(0,d.jsxs)(Mn,{children:[(0,d.jsx)(go,{}),(0,d.jsx)(f,{}),(0,d.jsx)(ps,{}),(0,d.jsx)(as,{})]})}));export{u as a,m as i,L as n,o,Nn as r,c as s,za as t};
